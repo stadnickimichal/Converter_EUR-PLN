08b13d6599b746835722d82865235dff60141581
tofik92d <stadnicki.michal@o2.pl>
2017-05-08 20:40:26 +0200

    wersja2

--- js/converte.js
+++ js/converte.js
@@ -3,20 +3,20 @@ document.addEventListener("DOMContentLoaded", function(){
         conv: $(".conv"),
         btn: $(".conv__btn")[0],
         
-        currentDate: function(){
+        currentDate(){
             var time= new Date();
             return time.getFullYear()+"."+time.getMonth()+"."+time.getDate()+" "+time.getHours()+":"+time.getMinutes();
         },
-        calculate: function(){
+        calculate(){
             var getValue= $(".conv__input").val();
             var rate= 4.2*(1+(Math.floor(Math.random()*100))/1000);
             return Math.floor(getValue*rate*100)/100;
         },
-        showResult: function(){
+        showResult(){
             this.conv.after("<p class='time'><i class='fa fa-clock-o' aria-hidden='true'></i> "+this.currentDate()+"</p>");
             this.conv.after("<div><div class='prev'><p>PLN AMOUNT</p>"+this.calculate()+"</div></div>");
         },
-        bind: function(){
+        bind(){
             this.btn.addEventListener('click',this.showResult.bind(this));
         }
     };
